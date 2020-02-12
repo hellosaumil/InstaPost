bb811ee910e8ca3085d96475139e8fb517410906
hellosaumil <6513922+hellosaumil@users.noreply.github.com>
2020-02-11 21:10:55 -0800

    Initial Commit

---
+++ InstaPost/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstaPost
+//
+//  Created by Saumil Shah on 2/11/20.
+//  Copyright © 2020 Saumil Shah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
